a3aa14197bd205cf6a26254d1a289f77982a7219
Uriel Estrada <v-u.estrada@pokemon.com>
2025-03-21 15:19:14 -0600

    [ADD]: Cron package
    Later on, decouple direct implementation into adapter patron

--- node-only/05-NOC/src/presentation/server.ts
+++ node-only/05-NOC/src/presentation/server.ts
@@ -1,5 +1,14 @@
+import { CronJob } from "cron";
+
 export class ServerApp {
   public static start() {
     console.log("Server started ...");
+
+    // See usage examples in: https://github.com/kelektiv/node-cron/tree/main/examples
+    const job = new CronJob("*/2 * * * * *", () => {
+      const date = new Date();
+      console.log("2 second", date);
+    });
+    job.start();
   }
 }
