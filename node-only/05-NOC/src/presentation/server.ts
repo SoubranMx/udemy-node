2b399d42264305fa5c9cef7cb0ef4e51f2cd72b2
Uriel Estrada <v-u.estrada@pokemon.com>
2025-03-24 10:26:43 -0600

    [UPD]: Implement simple dependency injection

--- node-only/05-NOC/src/presentation/server.ts
+++ node-only/05-NOC/src/presentation/server.ts
@@ -1,12 +1,23 @@
 import { CheckService } from "../domain/use-cases/checks/check-service.js";
 import { CronService } from "./cron/cron-service.js";
 
+function successInjection(url: string) {
+  console.log(`${url} is ok`);
+}
+
+function errorInjection(error: string) {
+  console.error(error);
+}
 export class ServerApp {
   public static start() {
     console.log("Server started ...");
 
-    CronService.createJob("*/5 * * * * *", () => {
-      new CheckService().execute("https://google.com");
+    const url = "https://google.com";
+    const jobTimer = "*/5 * * * * *";
+    CronService.createJob(jobTimer, () => {
+      new CheckService(() => successInjection(url), errorInjection).execute(
+        url
+      );
     });
   }
 }
