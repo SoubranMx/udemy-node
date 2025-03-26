ff20a21846bef9c5981c3ee8e12169a85b49c4a5
Uriel Soubran <uriel_bee15@hotmail.com>
2025-03-25 20:31:54 -0600

    [ADD]: Mongoose connection

--- node-only/05-NOC/src/app.ts
+++ node-only/05-NOC/src/app.ts
@@ -1,9 +1,16 @@
+import { envs } from "@config/plugins/envs.plugin.ts";
 import { ServerApp } from "@presentation/server.ts";
 import "dotenv/config";
-(() => {
+import { MongoDatabase } from "./data/mongo/index.ts";
+
+(async () => {
   main();
 })();
 
-function main() {
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
   ServerApp.start();
 }
